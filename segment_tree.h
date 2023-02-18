224785b2227363e9379b5ed277d14fe872a3f4c0
wkjfive <105552162+wkjfive@users.noreply.github.com>
2023-02-18 06:11:52 +0000

    给线段树添加复制构造函数和赋值运算符
    给线段树添加复制构造函数和赋值运算符，把缩进改为空格，修改函数名

--- segment_tree.h
+++ segment_tree.h
@@ -11,172 +11,172 @@
 typedef long long ll;
 
 class SegmentTree{
-	public:
-	typedef ll Tp;
-	private:
-	class Node{
-    	private:
-    	Tp sum;//区间和 
-    	Tp delta,dt2;//加法乘法延迟标记(先乘再加) 
-    	int lef,righ;//范围 
-    	Tp M;//模数 
-    	Node *ls,*rs;//左右孩子 
-    	public:
-    	Node(int l,int r,Tp Mod=1LL<<62,Tp* a=NULL);//建树 
-    	Node(const Node& x);
-    	Node& operator=(const Node& x);
+    public:
+    typedef ll Tp;
+    private:
+    class Node{
+        private:
+        Tp sum;//区间和 
+        Tp delta,dt2;//加法乘法延迟标记(先乘再加) 
+        int lef,righ;//范围 
+        Tp M;//模数 
+        Node *ls,*rs;//左右孩子 
+        public:
+        Node(int l,int r,Tp Mod=1LL<<62,Tp* a=NULL);//建树 
+        Node(const Node& x);
+        Node& operator=(const Node& x);
         #if defined(__cplusplus) && __cplusplus>=201100L
-    	Node(Node&& x);
-    	Node& operator=(Node&& x);
+        Node(Node&& x);
+        Node& operator=(Node&& x);
         #endif
         ~Node();
         
-    	void pushdown();//标记下传 
-    	void modify(int l,int r,Tp k);//区间加 
-    	void mutip(int l,int r,Tp k);//区间乘 
-    	Tp query(int l,int r);//区间求和 
+        void pushdown();//标记下传 
+        void modify(int l,int r,Tp k);//区间加 
+        void mutip(int l,int r,Tp k);//区间乘 
+        Tp query(int l,int r);//区间求和 
     };
-	Node *root;
-	public:
-	SegmentTree(int size,Tp Mod=1LL<<62,Tp* a=NULL){
+    Node *root;
+    public:
+    SegmentTree(int size,Tp Mod=1LL<<62,Tp* a=NULL){
         //initializer_list<Tp> val
         //Tp a[n];for(int i=0;i<size;i++) a[i]=val[i];
-    	root=new Node(1,size,Mod,a);
+        root=new Node(1,size,Mod,a);
     }
-	SegmentTree(const SegmentTree& x){
-    	root=new node(*x.root);
+    SegmentTree(const SegmentTree& x){
+        root=new node(*x.root);
     }
-	SegmentTree& operator=(const SegmentTree& x){
-    	delete root; root=new node(*x.root);
+    SegmentTree& operator=(const SegmentTree& x){
+        delete root; root=new node(*x.root);
     }
     #if defined(__cplusplus) && __cplusplus>=201100L
-	SegmentTree(SegmentTree&& x){
-    	root=x.root,x.root=NULL;
+    SegmentTree(SegmentTree&& x){
+        root=x.root,x.root=NULL;
     }
-	SegmentTree& operator=(SegmentTree&& x){
-    	delete(root); root=x.root,x.root=NULL;
+    SegmentTree& operator=(SegmentTree&& x){
+        delete(root); root=x.root,x.root=NULL;
     }
     #endif
     ~SegmentTree(){
-    	delete root;
+        delete root;
     }
     
-	void add(int l,int r,Tp k){//区间加 
-    	root->add(l,r,k);
+    void add(int l,int r,Tp k){//区间加 
+        root->add(l,r,k);
     }
-	void add(int x,int k){
-    	add(x,x,k);
+    void add(int x,int k){
+        add(x,x,k);
     }
-	void mutip(int l,int r,Tp k){//区间乘 
-    	root->mutip(l,r,k);
+    void mutip(int l,int r,Tp k){//区间乘 
+        root->mutip(l,r,k);
     }
-	void mutip(int x,Tp k){
-    	mutip(x,x,k);
+    void mutip(int x,Tp k){
+        mutip(x,x,k);
     }
-	Tp query(int l,int r){//区间求和 
-    	return root->query(l,r);
+    Tp query(int l,int r){//区间求和 
+        return root->query(l,r);
     }
 };
 
 SegmentTree::Node::Node(int l,int r,Tp Mod,Tp* a){//建树 
-	M=Mod;lef=l,righ=r;delta=0,dt2=1;ls=rs=NULL;
-	if(l==r){
-    	if(a==NULL) sum=0;
-    	else sum=a[l];
+    M=Mod;lef=l,righ=r;delta=0,dt2=1;ls=rs=NULL;
+    if(l==r){
+        if(a==NULL) sum=0;
+        else sum=a[l];
     }else{
-    	int m=(l+r)/2;
-    	if(a==NULL) sum=0;
-    	else{
-        	ls=new Node(l,m,M,a);
-        	rs=new Node(m+1,r,M,a);
-        	sum=(ls->sum)+(rs->sum);
+        int m=(l+r)/2;
+        if(a==NULL) sum=0;
+        else{
+            ls=new Node(l,m,M,a);
+            rs=new Node(m+1,r,M,a);
+            sum=(ls->sum)+(rs->sum);
         }
     } sum%=M;
 }
 SegmentTree::Node::Node(const Node& x){
-	sum=x.sum,delta=x.delta,dt2=x.dt2,lef=x.lef,righ=x.righ,M=x.M;
-	if(x.ls!=NULL) ls=new Node(*x.ls);
-	if(x.rs!=NULL) rs=new Node(*x.rs);
+    sum=x.sum,delta=x.delta,dt2=x.dt2,lef=x.lef,righ=x.righ,M=x.M;
+    if(x.ls!=NULL) ls=new Node(*x.ls);
+    if(x.rs!=NULL) rs=new Node(*x.rs);
 }
 SegmentTree::Node& SegmentTree::Node::operator=(const Node& x){
-	if(ls!=NULL) delete ls;
-	if(rs!=NULL) delete rs;
-	sum=x.sum,delta=x.delta,dt2=x.dt2,lef=x.lef,righ=x.righ,M=x.M;
-	if(x.ls!=NULL) ls=new Node(*x.ls);
-	if(x.rs!=NULL) rs=new Node(*x.rs);
+    if(ls!=NULL) delete ls;
+    if(rs!=NULL) delete rs;
+    sum=x.sum,delta=x.delta,dt2=x.dt2,lef=x.lef,righ=x.righ,M=x.M;
+    if(x.ls!=NULL) ls=new Node(*x.ls);
+    if(x.rs!=NULL) rs=new Node(*x.rs);
 }
 #if defined(__cplusplus) && __cplusplus>=201100L
 SegmentTree::Node::Node(Node&& x){
-	sum=x.sum,delta=x.delta,dt2=x.dt2,lef=x.lef,righ=x.righ,M=x.M;
-	ls=x.ls,x.ls=NULL; rs=x.rs,x.rs=NULL;
+    sum=x.sum,delta=x.delta,dt2=x.dt2,lef=x.lef,righ=x.righ,M=x.M;
+    ls=x.ls,x.ls=NULL; rs=x.rs,x.rs=NULL;
 }
 SegmentTree::Node::Node& SegmentTree::Node::operator=(Node&& x){
-	if(ls!=NULL) delete ls;
-	if(rs!=NULL) delete rs;
-	sum=x.sum,delta=x.delta,dt2=x.dt2,lef=x.lef,righ=x.righ,M=x.M;
-	ls=x.ls,x.ls=NULL;rs=x.rs,x.rs=NULL;
+    if(ls!=NULL) delete ls;
+    if(rs!=NULL) delete rs;
+    sum=x.sum,delta=x.delta,dt2=x.dt2,lef=x.lef,righ=x.righ,M=x.M;
+    ls=x.ls,x.ls=NULL;rs=x.rs,x.rs=NULL;
 }
 #endif
 SegmentTree::Node::~Node(){
-	if(ls!=NULL) delete ls;
-	if(rs!=NULL) delete rs;
+    if(ls!=NULL) delete ls;
+    if(rs!=NULL) delete rs;
 }
 
 void SegmentTree::Node::pushdown(){//标记下传 
-	int m=(lef+righ)/2;
+    int m=(lef+righ)/2;
     //左 
-	if(ls==NULL) ls=new Node(lef,m,M);
+    if(ls==NULL) ls=new Node(lef,m,M);
     ((ls->sum)*=dt2)%=M;
     ((ls->delta)*=dt2)%=M,((ls->dt2)*=dt2)%=M;
     ((ls->sum)+=delta*((ls->righ)-(ls->lef)+1))%=M;
     ((ls->delta)+=delta)%=M;
     //右 
-	if(rs==NULL) rs=new Node(m+1,righ,M);
+    if(rs==NULL) rs=new Node(m+1,righ,M);
     ((rs->sum)*=dt2)%=M;
     ((rs->delta)*=dt2)%=M,((rs->dt2)*=dt2)%=M;
     ((rs->sum)+=delta*((rs->righ)-(rs->lef)+1))%=M;
     ((rs->delta)+=delta)%=M;
     //删除标记 
-	delta=0; dt2=1;
+    delta=0; dt2=1;
 }
 void SegmentTree::Node::add(int l,int r,Tp k){//区间加 
-	k%=M;
-	if(lef==l&&righ==r){
+    k%=M;
+    if(lef==l&&righ==r){
         (sum+=k*(righ-lef+1))%=M;
         (delta+=k)%=M;
     }else{
-    	pushdown();
-    	int m=(lef+righ)/2;
-    	if(r<=m) ls->add(l,r,k);
-    	else if(l>=m+1) rs->add(l,r,k);
-    	else ls->add(l,m,k),rs->add(m+1,r,k);
-    	sum=(ls->sum)+(rs->sum);
+        pushdown();
+        int m=(lef+righ)/2;
+        if(r<=m) ls->add(l,r,k);
+        else if(l>=m+1) rs->add(l,r,k);
+        else ls->add(l,m,k),rs->add(m+1,r,k);
+        sum=(ls->sum)+(rs->sum);
     }
 }
 void SegmentTree::Node::mutip(int l,int r,Tp k){//区间乘 
-	k%=M;
-	if(lef==l&&righ==r){
+    k%=M;
+    if(lef==l&&righ==r){
         (sum*=k)%=M;
         (delta*=k)%=M,(dt2*=k)%=M; 
     }else{
-    	pushdown();
-    	int m=(lef+righ)/2;
-    	if(r<=m) ls->mutip(l,r,k);
-    	else if(l>=m+1) rs->mutip(l,r,k);
-    	else ls->mutip(l,m,k),rs->mutip(m+1,r,k);
-    	sum=(ls->sum)+(rs->sum);
+        pushdown();
+        int m=(lef+righ)/2;
+        if(r<=m) ls->mutip(l,r,k);
+        else if(l>=m+1) rs->mutip(l,r,k);
+        else ls->mutip(l,m,k),rs->mutip(m+1,r,k);
+        sum=(ls->sum)+(rs->sum);
     }
 }
 SegmentTree::Tp SegmentTree::Node::query(int l,int r){//区间求和     
-	if(lef==l&&righ==r){
-    	return sum;
+    if(lef==l&&righ==r){
+        return sum;
     }else{
-    	if(ls==NULL&&rs==NULL) return 0;
-    	pushdown();
-    	int m=(lef+righ)/2;
-    	if(r<=m) return ls->query(l,r);
-    	else if(l>=m+1) return rs->query(l,r);
-    	else return ((ls->query(l,m))+(rs->query(m+1,r)))%M;
+        if(ls==NULL&&rs==NULL) return 0;
+        pushdown();
+        int m=(lef+righ)/2;
+        if(r<=m) return ls->query(l,r);
+        else if(l>=m+1) return rs->query(l,r);
+        else return ((ls->query(l,m))+(rs->query(m+1,r)))%M;
     }
 }
 
